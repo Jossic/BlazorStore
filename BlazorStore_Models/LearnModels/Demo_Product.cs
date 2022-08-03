7a6d01c9cabbe36dbd87abbdcde2089d3518cc58
Jossic <jossic.lapierre@marvin-bot.com>
2022-08-03 15:52:35 +0200

    Feature: Add Demo product and data-bind

---
+++ BlazorStore_Models/LearnModels/Demo_Product.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorStore_Models.LearnModels;
+
+public class Demo_Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public bool IsActive { get; set; }
+}
+
