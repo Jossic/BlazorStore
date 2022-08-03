b53d5ce0a73bb0f5e15669f63d3162fdcddc09f8
Jossic <jossic.lapierre@marvin-bot.com>
2022-08-03 16:43:31 +0200

    Feature: Add select

---
+++ BlazorStore_Models/LearnModels/Demo_ProductProp.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorStore_Models.LearnModels
+{
+    public class Demo_ProductProp
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
