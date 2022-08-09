45b879b6b29a66e642c7ff1e1e86ba2687eba8a8
Jossic <jossic.lapierre@gmail.com>
2022-08-09 20:52:33 +0200

    Feature: Add Category Repository

---
+++ BlazoeStore_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BlazorStore_Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazoeStore_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public Create(CategoryDto objDto)
+    }
+}
