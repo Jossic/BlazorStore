657c821fc5813efedb32e2864a5097f30668717b
Jossic <jossic.lapierre@marvin-bot.com>
2022-08-09 14:05:43 +0200

    Feature: Add categoryDto

--- BlazorStore_DataAccess/Category.cs
+++ BlazorStore_DataAccess/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace BlazorStore_DataAccess
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorStore_DataAccess
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
