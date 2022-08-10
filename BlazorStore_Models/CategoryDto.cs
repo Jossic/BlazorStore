4b21cdde5f502e7cf81a6c76b57f00e3c43d473e
Jossic <jossic.lapierre@marvin-bot.com>
2022-08-10 13:42:50 +0200

    Feature - Add Form Control

--- BlazorStore_Models/CategoryDto.cs
+++ BlazorStore_Models/CategoryDto.cs
@@ -1,7 +1,10 @@
-﻿namespace BlazorStore_Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorStore_Models;
 
 public class CategoryDto
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Rentre un nom...")]
     public string Name { get; set; }
 }
